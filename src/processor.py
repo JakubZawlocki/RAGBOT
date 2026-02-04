80b5eac0fb091c958db20e78f2b67430a4f0767f
Piotr Pawlak <Piotrpawlak96@gmail.com>
2026-02-04 22:26:31 +0100

    new statistics and metadata for pdf processing and updated .gitignore

--- src/processor.py
+++ src/processor.py
@@ -1,4 +1,5 @@
 import os
+import statistics
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -12,13 +13,28 @@ class DocumentProcessor:
     def process_pdf(self, file_path):
         loader = PyPDFLoader(file_path)
         pages = loader.load()
-        
+        file_name = os.path.basename(file_path)
+
+
+        for i, pdf in enumerate(pages):
+            pdf.metadata["source"] = file_name
+            pdf.metadata["page"] = int(pdf.metadata.get("page", i)) + 1
+
+
         # Statystyki do Punktu 4
+        chunks = self.splitter.split_documents(pages)
+        chunks = [chunk for chunk in chunks if chunk.page_content and chunk.page_content.strip()] # likwidacja chunków wypełnionych pustymi znakami
+        chunk_lengths = [len(chunk.page_content) for chunk in chunks]
+
         stats = {
-            "filename": os.path.basename(file_path),
+            "filename": file_name,
             "pages": len(pages),
-            "total_chars": sum(len(p.page_content) for p in pages)
+            "total_chars": sum(len(p.page_content) for p in pages),
+            "num_chunks": len(chunks),
+            "chunk_min": min(chunk_lengths) if chunk_lengths else 0,
+            "chunk_avg": int(sum(chunk_lengths) / len(chunk_lengths)) if chunk_lengths else 0,
+            "chunk_median": int(statistics.median(chunk_lengths)) if chunk_lengths else 0,
+            "chunk_max": max(chunk_lengths) if chunk_lengths else 0
         }
         
-        chunks = self.splitter.split_documents(pages)
         return chunks, stats
